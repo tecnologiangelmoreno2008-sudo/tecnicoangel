3052a5185dacc0b097c00f1eb65d29af133bfc2b
Angel <tecnologiangelmoreno2008@gmail.com>
2026-03-20 17:34:58 -0500

    los primeros archivos

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,16 @@
+/*
+let numero1 = prompt("ingrese el primer numero");
+
+
+let numero2 = prompt("ingrese el segundo  numero");
+
+
+numero1 = parseFloat(numero1);
+numero2 = parseFloat(numero2);
+
+
+alert(numero1 + numero2);
+*/
+  window.onload = function() {
+    alert("Bienvenido a Técnico Angeles mas conocido como TecnicoAngel");
+}
