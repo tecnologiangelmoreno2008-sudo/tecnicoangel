8e917921ba73915c3fec81d004dbe858e76deb83
Angel <tecnologiangelmoreno2008@gmail.com>
2026-03-24 18:11:13 -0500

    los primeros archivos

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -11,6 +11,31 @@ numero2 = parseFloat(numero2);
 
 alert(numero1 + numero2);
 */
-  window.onload = function() {
-    alert("Bienvenido a Técnico Angeles mas conocido como TecnicoAngel");
+
+/*
+let usuario = prompt("nombre de usuario")
+let password = prompt("Ingrese su contraseña");
+let role = prompt("Ingrese su rol ( Administrador, editor,Invitado):")
+
+if (role === "administrador") {
+  alert("Bienvenido, " + user + ". Tienes aceceso completo.");
+} else if (role === "Editor") {
+  alert("Bienvenido, " + user + ". Puiedes editar contenido.");
+} else {
+  alert("Bienvenido, " + user + ". Tienes aceceso limitado.");
+}
+  */
+window.onload = function () {
+  alert("Bienvenido a Técnico Angeles mas conocido como TecnicoAngel");
+}
+
+let edad = prompt("Ingrese su edad");
+edad = parseInt(edad);
+
+if (isNaN(edad)) {
+  alert("Por favor ingrese un número válido");
+} else if (edad >= 18) {
+  alert("Eres mayor de edad");
+} else {
+  alert("Eres menor de edad");
 }
